dc6aae36a7cfccb0d2d30b0a90c04dd9cb36ee4b
Veeramani <veeramani@neverskip.com>
2024-01-05 17:36:20 +0530

    adding route

---
+++ app/article/[...slug]/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+import { paramsType } from '@/app/types/project.type'
+
+export default function Page({ params }:{ params: paramsType }) {
+    
+    return <div>
+        <h1>{params.slug}</h1>
+    </div>
+}
