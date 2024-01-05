0cb09dc2822fba69f1ac2b8c2ef8e26c32f1cff5
Pavan <pavandec1993@gmail.com>
2024-01-05 23:29:26 +0530

    home page content

---
+++ app/contactus/page.tsx
@@ -0,0 +1,9 @@
+// import Image from "next/image";
+
+export default function ContactUs() {
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<h1>Contact Us Page ❤</h1>
+		</main>
+	);
+}
